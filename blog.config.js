99910a7680ac0357fa203ecca18b8a5d45612c9a
illegiblescholar <48851266+illegiblescholar@users.noreply.github.com>
2022-01-09 18:26:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/DaYu___zhang',
+  socialLink: 'https://twitter.com/liaocaoxuezhe',
   seo: {
     keywords: ['Blog', 'Website', 'Notion','潦草学者'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
